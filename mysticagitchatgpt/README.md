784f0627e3a0ff86daa08a0dac2e02994058ddda
jschoi <jschoi@com2us.com>
2023-05-17 13:01:10 +0900

    - update app ver 1.2 - update UI (modify request text array) - update README

---
+++ mysticagitchatgpt/README.md
@@ -0,0 +1,19 @@
+# ChatGPTSample
+
+빌드 테스트 환경
+- 2023.05 ~
+- Android Studio Electric Eel 2022.1.1 Patch 2
+- Android 12 Device test
+- TargetSDK 33
+- Sample ver : 1.2
+
+애플리케이션 실행 후 UI 노출 결과에 따른 기능
+- 질문 입력 textview
+  - ChatGPT 문의하려는 문장 입력
+- ChatGPT 검색
+  - ChatGPT에 문의 문장 질의
+- answer textview
+  - ChatGPT에서 응답된 문자열 token 나열
+
+개발 블로그
+- https://mystic-agit.tistory.com/5
--- mysticagitchatgpt/app/src/main/java/com/example/mysticagitchatgpt/MainActivity.kt
+++ mysticagitchatgpt/app/src/main/java/com/example/mysticagitchatgpt/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.mysticagitchatgpt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -14,15 +18,40 @@ class MainActivity : AppCompatActivity() {
     val HttpContentType_JSON: MediaType = "application/json; charset=utf-8".toMediaType()
     var okHttpClient = OkHttpClient()
 
+    val responseHandler: Handler = Handler {
+        var responseText: String? = it.obj.toString()
+
+        responseText?.let {
+            var tvResponseText = findViewById<TextView>(R.id.tv_response_chatgpt)
+            tvResponseText.text = it
+        }
+
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 메서드 호출
-        requestChatGPT("what is apple?")
+        // UI 구성
+        initUI()
     }
 
-    fun requestChatGPT(question: String) {
+    private fun initUI() {
+        var etRequestText = findViewById<EditText>(R.id.et_input_text)
+        var btRequest = findViewById<Button>(R.id.bt_request_chatgpt)
+
+
+        btRequest.setOnClickListener {
+            var requestText: String? = etRequestText.text.toString()
+
+            requestText?.let {
+                requestChatGPT(it)
+            }
+        }
+    }
+
+    private fun requestChatGPT(question: String) {
 
         Log.d("mysticagit", "question : $question")
 
@@ -32,17 +61,19 @@ class MainActivity : AppCompatActivity() {
         // API 레퍼런스 참고 ( https://platform.openai.com/docs/api-reference/completions/create )
         jsonBody.put("model", "text-davinci-003")
         jsonBody.put("prompt", question)
-        jsonBody.put("max_tokens", 30)
+        jsonBody.put("max_tokens", 30)      // 응답 문자열 token 수
         jsonBody.put("temperature", 0)
 
         // request에 필요한 요청 url(url), API Key(header) 설정
         // (API Key 확인하기 : https://platform.openai.com/account/api-keys )
+        var chatGPTApiKey = resources.getString(R.string.chatgpt_api_key)
         var requestBody: RequestBody = (jsonBody.toString()).toRequestBody(HttpContentType_JSON)
         var request: Request = Request.Builder()
             .url("https://api.openai.com/v1/completions")
-            .header("Authorization", "Bearer sk-AbcDeAbcdeABCDE1234567890J1234567890")  // API Key 발급받은 것을 사용하세요!
+            .header("Authorization", chatGPTApiKey)  // API Key 발급받은 것을 사용하세요!
             .post(requestBody)
             .build()
+        // example Authorization : Bearer sk-asdf1234qwer1234zxcv9876...
 
         // 요청
         okHttpClient.newCall(request).enqueue( object : Callback {
@@ -59,8 +90,10 @@ class MainActivity : AppCompatActivity() {
                     // 결과 출력
                     result?.let {
                         Log.d("mysticagit", "answer : $it")
+                        responseHandler.obtainMessage(1, -1, -1, it).sendToTarget()
                     } ?: {
                         Log.d("mysticagit", "answer is empty")
+                        responseHandler.obtainMessage(1, -1, -1, "(answer is empty)").sendToTarget()
                     }
                 }
             }
@@ -68,6 +101,7 @@ class MainActivity : AppCompatActivity() {
             // 응답 실패 시
             override fun onFailure(call: Call, e: IOException) {
                 Log.d("mysticagit", "onFailure")
+                responseHandler.obtainMessage(1, -1, -1, "(onFailure)").sendToTarget()
             }
         })
     }
